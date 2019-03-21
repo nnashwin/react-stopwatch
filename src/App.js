93289822f394b5170a1843000f8da03030ba6445
Tyler Boright <lupin3.ken@gmail.com>
2019-03-20 17:31:38 -0700

    master adds two onClick methods to the buttons, start and stop;  modifies the useEffect to request the AnimationFrame recursively if running

--- src/App.js
+++ src/App.js
@@ -2,31 +2,70 @@ import './App.css'
 
 import React, { useState, useEffect, useRef } from 'react'
 
+const pad0 = (value, count) => {
+    var result = value.toString();
+    for (; result.length < count; --count)
+        result = '0' + result;
+    return result;
+}
+
 function App() {
   const [times, setTimes] = useState([0, 0, 0]);
+  const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [globalID, setGlobalID] = useState(0);
 
   const stopRef = useRef();
 
-  const print = () => {
-    stopRef.current.innerText = times;
+  const format = (times) => {
+      return `\
+        ${pad0(times[0], 2)}:\
+        ${pad0(times[1], 2)}:\
+        ${pad0(Math.floor(times[2]), 2)}
+      `;
+    }
+
+  const calculate = (timestamp) => {
+    let diff = timestamp - time;
+    setTime(timestamp)
   }
 
+  const start = () => {
+    if (!time) setTime(performance.now());
+    if (!isRunning) {
+      setIsRunning(true);
+      setGlobalID(requestAnimationFrame(step.bind(this)));
+    }
+  }
+
+  const step = (timestamp) => {
+    if (!isRunning) return;
+    if (isRunning) {
+      requestAnimationFrame(step);
+    }
+  }
+
+  const stop = () => {
+    setIsRunning(false)
+    cancelAnimationFrame(globalID);
+  };
+
   useEffect(() => {
-  
-    print();
-  });
-  
+    if (isRunning) {
+      requestAnimationFrame(setTime);
+    }
+  })
+
   return (
     <div>
       <nav className="controls">
-        <a href="#" className="button">
+        <a href="#" className="button" onClick={start}>
           Start
         </a>
         <a href="#">
           Lap
         </a>
-        <a href="#">
+        <a href="#" className="button" onClick={stop}>
           Stop
         </a>
         <a href="#">
@@ -37,7 +76,7 @@ function App() {
         </a>
       </nav>
       <div ref={stopRef} className="Stopwatch"></div>
-      <ul className="results"></ul>
+      <ul className="results">{format(times)}</ul>
     </div>
   );
 }
